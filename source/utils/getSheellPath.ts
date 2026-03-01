c07104c99b8ade151dd36c8dda53f3da048db29b
Lysyonok Gregory <bloodamerika@gmail.com>
2026-03-01 19:42:41 +0300

    utilitie are separated

--- source/utils/getSheellPath.ts
+++
@@ -1,8 +0,0 @@
-import {fileURLToPath} from 'url';
-import {dirname, resolve} from 'path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-export function getShellPath(file: string) {
-	return resolve(__dirname, '..', 'shell', file);
-}
